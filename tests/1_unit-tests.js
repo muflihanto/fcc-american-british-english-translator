b9ffb9abee32f3cd1d6687e9fb40972820ad8225
muflihanto <muflihanto@gmail.com>
2023-12-04 18:43:18 +0700

    test: add todo notes

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,8 +1,31 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
-suite('Unit Tests', () => {
-
+suite("Unit Tests", () => {
+  // TODO: Translate Mangoes are my favorite fruit. to British English
+  // TODO: Translate I ate yogurt for breakfast. to British English
+  // TODO: Translate We had a party at my friend's condo. to British English
+  // TODO: Translate Can you toss this in the trashcan for me? to British English
+  // TODO: Translate The parking lot was full. to British English
+  // TODO: Translate Like a high tech Rube Goldberg machine. to British English
+  // TODO: Translate To play hooky means to skip class or work. to British English
+  // TODO: Translate No Mr. Bond, I expect you to die. to British English
+  // TODO: Translate Dr. Grosh will see you now. to British English
+  // TODO: Translate Lunch is at 12:15 today. to British English
+  // TODO: Translate We watched the footie match for a while. to American English
+  // TODO: Translate Paracetamol takes up to an hour to work. to American English
+  // TODO: Translate First, caramelise the onions. to American English
+  // TODO: Translate I spent the bank holiday at the funfair. to American English
+  // TODO: Translate I had a bicky then went to the chippy. to American English
+  // TODO: Translate I've just got bits and bobs in my bum bag. to American English
+  // TODO: Translate The car boot sale at Boxted Airfield was called off. to American English
+  // TODO: Translate Have you met Mrs Kalyani? to American English
+  // TODO: Translate Prof Joyner of King's College, London. to American English
+  // TODO: Translate Tea time is usually around 4 or 4.30. to American English
+  // TODO: Highlight translation in Mangoes are my favorite fruit.
+  // TODO: Highlight translation in I ate yogurt for breakfast.
+  // TODO: Highlight translation in We watched the footie match for a while.
+  // TODO: Highlight translation in Paracetamol takes up to an hour to work.
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,12 +1,17 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 const assert = chai.assert;
-const server = require('../server.js');
+const server = require("../server.js");
 
 chai.use(chaiHttp);
 
-let Translator = require('../components/translator.js');
+let Translator = require("../components/translator.js");
 
-suite('Functional Tests', () => {
-
+suite("Functional Tests", () => {
+  // TODO: Translation with text and locale fields: POST request to /api/translate
+  // TODO: Translation with text and invalid locale field: POST request to /api/translate
+  // TODO: Translation with missing text field: POST request to /api/translate
+  // TODO: Translation with missing locale field: POST request to /api/translate
+  // TODO: Translation with empty text: POST request to /api/translate
+  // TODO: Translation with text that needs no translation: POST request to /api/translate
 });
