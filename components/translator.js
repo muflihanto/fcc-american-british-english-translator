9295fc98830e6c57495227d816f1b30ca9111c89
muflihanto <muflihanto@gmail.com>
2023-12-04 19:29:28 +0700

    add initial translator and api handler

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,61 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
+  #britishDict = Object.assign(
+    {},
+    britishOnly,
+    ...[americanOnly, americanToBritishSpelling, americanToBritishTitles].map((el) =>
+      this.#swapKeysAndValues(el),
+    ),
+  );
 
+  #americanDict = Object.assign(
+    {},
+    americanOnly,
+    americanToBritishSpelling,
+    americanToBritishTitles,
+    this.#swapKeysAndValues(britishOnly),
+  );
+
+  #swapKeysAndValues(obj) {
+    const swapped = Object.entries(obj).map(([key, value]) => [value, key]);
+
+    return Object.fromEntries(swapped);
+  }
+
+  translate(text, locale) {
+    let result;
+    if (locale === "american-to-british") {
+      result = this.handleAmericanToBritish(text);
+    } else if (locale === "british-to-american") {
+      result = this.handleBritishToAmerican(text);
+    }
+    return text === result ? "Everything looks good to me!" : result;
+  }
+
+  handleBritishToAmerican(text) {
+    const parsedText = text.split(/([\s\W])/);
+    const translated = parsedText.map((txt) => {
+      if (this.#britishDict.hasOwnProperty(txt))
+        return `<span class="highlight">${this.#britishDict[txt]}</span>`;
+      return txt;
+    });
+    return translated.join("");
+  }
+
+  handleAmericanToBritish(text) {
+    const parsedText = text.split(/([\s\W])/);
+    console.log({ parsedText });
+    const translated = parsedText.map((txt) => {
+      if (this.#americanDict.hasOwnProperty(txt))
+        return `<span class="highlight">${this.#americanDict[txt]}</span>`;
+      return txt;
+    });
+    return translated.join("");
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,18 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
+  const locales = ["american-to-british", "british-to-american"];
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    const { text, locale } = req.body;
+    if (text === undefined || locale === undefined)
+      return res.json({ error: "Required field(s) missing" });
+    if (text === "") return res.json({ error: "No text to translate" });
+    if (!locales.includes(locale)) return res.send({ error: "Invalid value for locale field" });
+    const translation = translator.translate(text, locale);
+    res.json({ text, translation });
+  });
 };
