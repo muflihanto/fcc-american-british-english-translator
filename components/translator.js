036969041f8a6b60617127fda31f6b7e58314b3f
muflihanto <muflihanto@gmail.com>
2023-12-05 21:31:34 +0700

    add time handler for translator

--- components/translator.js
+++ components/translator.js
@@ -3,15 +3,15 @@ const americanToBritishSpelling = require("./american-to-british-spelling.js");
 const americanToBritishTitles = require("./american-to-british-titles.js");
 const britishOnly = require("./british-only.js");
 
-// TODO: add time and title handler
+// TODO: add title handler
 
 class Translator {
   #britishDict = Object.assign(
     {},
     britishOnly,
-    ...[americanOnly, americanToBritishSpelling, americanToBritishTitles].map(
-      (el) => this.#swapKeysAndValues(el)
-    )
+    ...[americanOnly, americanToBritishSpelling, americanToBritishTitles].map((el) =>
+      this.#swapKeysAndValues(el),
+    ),
   );
 
   #americanDict = Object.assign(
@@ -19,13 +19,34 @@ class Translator {
     americanOnly,
     americanToBritishSpelling,
     americanToBritishTitles,
-    this.#swapKeysAndValues(britishOnly)
+    this.#swapKeysAndValues(britishOnly),
   );
 
   #swapKeysAndValues(obj) {
     const swapped = Object.entries(obj).map(([key, value]) => [value, key]);
 
     return Object.fromEntries(swapped);
+  }
+
+  #highlightTime(text, searchValue, replaceValue) {
+    const timeRegex = new RegExp(
+      "(([01]?[0-9]||[2][0-4])" + `${searchValue === "." ? "\\." : searchValue}` + "\\d\\d)",
+    );
+    const matchTime = text.match(timeRegex);
+    if (
+      matchTime !== null &&
+      matchTime[0] !== undefined &&
+      (matchTime[0].length === 4 || matchTime[0].length === 5)
+    ) {
+      const time = matchTime[0];
+      const timeIndex = text.indexOf(time);
+      return (
+        text.slice(0, timeIndex) +
+        `<span class="highlight">${time.replace(searchValue, replaceValue)}</span>` +
+        text.slice(time.length + timeIndex)
+      );
+    }
+    return text;
   }
 
   translate(text, locale) {
@@ -40,23 +61,28 @@ class Translator {
 
   handleBritishToAmerican(text) {
     const parsedText = text.split(/([\s\W])/);
-    const translated = parsedText.map((txt) => {
-      if (this.#britishDict.hasOwnProperty(txt))
-        return `<span class="highlight">${this.#britishDict[txt]}</span>`;
-      return txt;
-    });
-    return translated.join("");
+    let translated = parsedText
+      .map((txt) => {
+        if (this.#britishDict.hasOwnProperty(txt))
+          return `<span class="highlight">${this.#britishDict[txt]}</span>`;
+        return txt;
+      })
+      .join("");
+    translated = this.#highlightTime(translated, ".", ":");
+    return translated;
   }
 
   handleAmericanToBritish(text) {
     const parsedText = text.split(/([\s\W])/);
-    console.log({ parsedText });
-    const translated = parsedText.map((txt) => {
-      if (this.#americanDict.hasOwnProperty(txt))
-        return `<span class="highlight">${this.#americanDict[txt]}</span>`;
-      return txt;
-    });
-    return translated.join("");
+    let translated = parsedText
+      .map((txt) => {
+        if (this.#americanDict.hasOwnProperty(txt))
+          return `<span class="highlight">${this.#americanDict[txt]}</span>`;
+        return txt;
+      })
+      .join("");
+    translated = this.#highlightTime(translated, ":", ".");
+    return translated;
   }
 }
 
