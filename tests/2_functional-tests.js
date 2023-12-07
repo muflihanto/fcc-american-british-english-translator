fe024982ec332a46b102a6bf8244f0b78aeb7755
muflihanto <muflihanto@gmail.com>
2023-12-07 15:04:09 +0700

    test: complete functional-tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,10 +8,136 @@ chai.use(chaiHttp);
 let Translator = require("../components/translator.js");
 
 suite("Functional Tests", () => {
-  // TODO: Translation with text and locale fields: POST request to /api/translate
-  // TODO: Translation with text and invalid locale field: POST request to /api/translate
-  // TODO: Translation with missing text field: POST request to /api/translate
-  // TODO: Translation with missing locale field: POST request to /api/translate
-  // TODO: Translation with empty text: POST request to /api/translate
-  // TODO: Translation with text that needs no translation: POST request to /api/translate
+  const url = "/api/translate";
+  const translator = new Translator();
+  // Translation with text and locale fields: POST request to /api/translate
+  test("Translation with text and locale fields: POST request to /api/translate", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .post(url)
+      .send({
+        text: "Mangoes are my favorite fruit.",
+        locale: "american-to-british",
+      })
+      .end(function (_, res) {
+        assert.equal(res.status, 200);
+        const result = JSON.parse(res.text);
+        assert.strictEqual(result.text, "Mangoes are my favorite fruit.");
+        assert.strictEqual(
+          result.translation,
+          translator.translate("Mangoes are my favorite fruit.", "american-to-british"),
+        );
+      });
+
+    chai
+      .request(server)
+      .keepOpen()
+      .post(url)
+      .send({
+        text: "Paracetamol takes up to an hour to work.",
+        locale: "british-to-american",
+      })
+      .end(function (_, res) {
+        assert.equal(res.status, 200);
+        const result = JSON.parse(res.text);
+        assert.strictEqual(result.text, "Paracetamol takes up to an hour to work.");
+        assert.strictEqual(
+          result.translation,
+          translator.translate("Paracetamol takes up to an hour to work.", "british-to-american"),
+        );
+      });
+
+    done();
+  });
+  // Translation with text and invalid locale field: POST request to /api/translate
+  test("Translation with text and invalid locale field: POST request to /api/translatee", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .post(url)
+      .send({
+        text: "Mangoes are my favorite fruit.",
+        locale: "american",
+      })
+      .end(function (_, res) {
+        assert.equal(res.status, 200);
+        const result = JSON.parse(res.text);
+        assert.deepEqual(result, { error: "Invalid value for locale field" });
+        done();
+      });
+  });
+  // Translation with missing text field: POST request to /api/translate
+  test("Translation with missing text field: POST request to /api/translate", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .post(url)
+      .send({
+        locale: "american-to-british",
+      })
+      .end(function (_, res) {
+        assert.equal(res.status, 200);
+        const result = JSON.parse(res.text);
+        assert.deepEqual(result, { error: "Required field(s) missing" });
+        done();
+      });
+  });
+  // Translation with missing locale field: POST request to /api/translate
+  test("Translation with missing locale field: POST request to /api/translate", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .post(url)
+      .send({
+        text: "Mangoes are my favorite fruit.",
+      })
+      .end(function (_, res) {
+        assert.equal(res.status, 200);
+        const result = JSON.parse(res.text);
+        assert.deepEqual(result, { error: "Required field(s) missing" });
+        done();
+      });
+  });
+  // Translation with empty text: POST request to /api/translate
+  test("Translation with missing locale field: POST request to /api/translate", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .post(url)
+      .send({
+        text: "",
+        locale: "american-to-british",
+      })
+      .end(function (_, res) {
+        assert.equal(res.status, 200);
+        const result = JSON.parse(res.text);
+        assert.deepEqual(result, { error: "No text to translate" });
+        done();
+      });
+  });
+  // Translation with text that needs no translation: POST request to /api/translate
+  test("Translation with text that needs no translation: POST request to /api/translate", (done) => {
+    chai
+      .request(server)
+      .keepOpen()
+      .post(url)
+      .send({
+        text: "Translation with text that needs no translation",
+        locale: "american-to-british",
+      })
+      .end(function (_, res) {
+        assert.equal(res.status, 200);
+        const result = JSON.parse(res.text);
+        const translatorResult = translator.translate(
+          "Translation with text that needs no translation",
+          "american-to-british",
+        );
+        assert.strictEqual(result.text, "Translation with text that needs no translation");
+        assert.strictEqual("Everything looks good to me!", result.translation);
+        assert.strictEqual("Everything looks good to me!", translatorResult);
+        assert.strictEqual(result.translation, translatorResult);
+        done();
+      });
+  });
 });
